9d92a30b06df1256460d1b0f67566060c7b1136a
BadtzMaru <707380414@qq.com>
2021-01-17 13:22:12 +0800

    配置react-native-vector-icons 矢量图标

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
 
+console.disableYellowBox = true;
+
 AppRegistry.registerComponent(appName, () => App);
